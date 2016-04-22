a306fa59b10f5ce6b0d2fc1d8036ec9841b2dd1c
Shane <shanodaviso@gmail.com>
2016-04-22 11:11:05 +0100

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn herokutheme.wsgi:application
---
+++ herokustrap/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class HerokustrapConfig(AppConfig):
+    name = 'herokustrap'
